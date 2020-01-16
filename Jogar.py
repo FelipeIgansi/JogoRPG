9fddf85c3cf3d9d6f2b2a8bd2787257f1502429a
FelipeIgansi <felipeigansi2009@hotmail.com>
2020-01-16 17:41:11 -0300

    Coloquei um menu e retirei os outpus de teste

--- Jogar.py
+++ Jogar.py
@@ -5,27 +5,36 @@ from random import randrange
 
 p1 = Personagem_Class('Gerald', 1000, 356, 689, 521)
 ListChars = []
-
-for i in range(100):
+op = 0
+for i in range(5):
     #nome = input('Qual é o nome do personagem?   ')
     nome = "Personagem "+str(randrange(0,100))
     vida = randrange(0, 101)
-    print('O valor de vida do personagem é:  ', vida)
     mana = randrange(500, 1500)
-    print('O valor de mana do personagem é:  ', mana)
     forca = randrange(500, 1500)
-    print('O valor de força do personagem é:  ', forca)
     defesa = randrange(500, 1500)
-    print('O valor de defesa do personagem é:  ', defesa)
-    print('\n\n')
 
     ListChars.append([Personagem_Class(nome, vida, mana, forca, defesa)])
 
-for lista in ListChars:
-    for col in lista:
-        print("Nome:  ", col.nome)
-        hp = col.MostrarVida(col.vida)
-        print("HP:  ", hp)
-        print("Mana:  ", col.mana)
-        print("Força:  ", col.forca)
-        print("Defesa:  ", col.defesa, "\n\n")
+
+while(op < 6):
+    print("Escolha uma das opções desejadas:  ")
+    print("   1 - Cadastrar personagem")
+    print("   2 - Matar personagem")
+    print("   3 - Atacar")
+    print("   4 - Defender")
+    print("   5 - Disparar feitiço")
+    print("   6 - Sair")
+
+    op = int(input('Qual é a opção desejada (escolha apenas o numero)?  '))
+
+    if(op == 1):
+        pass
+    elif(op == 2):
+        pass
+    elif(op == 3):
+        pass
+    elif(op == 4):
+        pass
+    elif(op == 5):
+        pass
